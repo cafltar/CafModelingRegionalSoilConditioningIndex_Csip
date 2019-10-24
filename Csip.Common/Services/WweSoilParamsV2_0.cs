9cf61314fcd90de01b5d84e03d9f954f7648accd
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-10-24 15:55:40 -0700

    init

---
+++ Csip.Common/Services/WweSoilParamsV2_0.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Common.Services
+{
+    class WweSoilParamsV2_0
+    {
+    }
+}
---
+++ Csip.Scenario/ScenarioBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Scenario
+{
+    class ScenarioBuilder
+    {
+    }
+}
