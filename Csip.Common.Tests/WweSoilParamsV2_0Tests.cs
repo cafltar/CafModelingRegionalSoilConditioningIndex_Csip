62f752cd36905f9cff311b480e9720236b5a4192
Bryan Carlson <bryan.r.carlson@gmail.com>
2019-10-25 14:12:49 -0700

    Added feature to request cokey from polygon

---
+++ Csip.Common.Tests/WweSoilParamsV2_0Tests.cs
@@ -0,0 +1,42 @@
+using Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Common.Services;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace Csip.Common.Tests
+{
+    public class WweSoilParamsV2_0Tests
+    {
+        readonly string jsonPolygonValid = "[[-118.67213507701905,45.71042895488162],[-118.67213507701905,45.74640181911838],[-118.62060247098096,45.74640181911838],[-118.62060247098096,45.71042895488162],[-118.67213507701905,45.71042895488162]]";
+        HttpClient client = new HttpClient();
+
+        [Fact]
+        public void BuildJsonContent_ValidJson_ExpectedResult()
+        {
+            // Arrange
+            //HttpClient client = new HttpClient();
+            var sut = new WweSoilParamsV2_0(client);
+
+            string expected = @"{""metainfo"":{},""parameter"":[{""name"":""aoa_geometry"",""type"":""Polygon"",""coordinates"":[[[-118.67213507701905,45.71042895488162],[-118.67213507701905,45.74640181911838],[-118.62060247098096,45.74640181911838],[-118.62060247098096,45.71042895488162],[-118.67213507701905,45.71042895488162]]]}]}";
+
+            // Act
+            string actual = sut.BuildJsonContent(jsonPolygonValid);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async void Post_ValidJson_Success()
+        {
+            // Arrange
+            var sut = new WweSoilParamsV2_0(client);
+
+            // Act
+            string actual = await sut.Post(jsonPolygonValid);
+
+            // Assert
+            Assert.True(!String.IsNullOrEmpty(actual));
+        }
+    }
+}
--- Csip.Common/Services/WweSoilParamsV2_0.cs
+++ Csip.Common/Services/WweSoilParamsV2_0.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Common.Services
 {
-    class WweSoilParamsV2_0
+    
+    public class WweSoilParamsV2_0
     {
+        private readonly HttpClient client;
+        private readonly string endpointPath;
+
+        public WweSoilParamsV2_0(HttpClient client)
+        {
+            this.client = client;
+            this.client.BaseAddress = new Uri(
+                "http://csip.engr.colostate.edu:8083/");
+            this.endpointPath = "csip-soils/d/wwesoilparams/2.0";
+        }
+
+        public async Task<string> Post(string jsonPolygon)
+        {
+            string jsonContent = BuildJsonContent(jsonPolygon);
+            HttpContent httpContent = new StringContent(
+                jsonContent, Encoding.UTF8, "application/json");
+
+            
+            var response = await client.PostAsync(endpointPath, httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                return result;
+            }
+            else
+            {
+                throw new Exception(
+                    $"Http request failed: {response.StatusCode.ToString()}");
+            }  
+        }
+
+        public string BuildJsonContent(string jsonPolygon)
+        {
+            // TODO: Probably should create models to serialize
+            //string jsonContent = $"\{\"metainfo\":\{\}";
+            string jsonContent = new StringBuilder()
+                .Append(@"{""metainfo"":{},""parameter"":[{""name"":""aoa_geometry"",""type"":""Polygon"",""coordinates"":[")
+                .Append(jsonPolygon)
+                .Append(@"]}]}")
+                .ToString();
+
+            return jsonContent;
+        }
     }
 }
