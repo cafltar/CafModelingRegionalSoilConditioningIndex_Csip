78c9afb21390477435f34049484df5085b5955a1
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-01-09 09:54:20 -0800

    Added feature to create SCI scenarios from ErosionParameters

---
+++ src/dotnet/Csip.Scenario.Tests/SciBuilderTest.cs
@@ -0,0 +1,52 @@
+﻿using Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Common.Models.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Scenario.Tests
+{
+    public class SciBuilderTest
+    {
+        [Fact]
+        public void BuildScenario_ValidInput_ExpectedResults()
+        {
+            // Arrange
+            var sut = new SciBuilder();
+            var input = GetErosionParametersMockValid();
+
+            // Act
+            var actual = sut.BuildScenarios(
+                input,
+                sut.GetTemplate());
+
+            // Assert
+            Assert.Single(actual);
+            Assert.Contains("47.0531__-117.2407__Transition_NoTill", actual.First());
+
+        }
+
+        private List<ErosionParameters> GetErosionParametersMockValid()
+        {
+            List<ErosionParameters> parameters =
+                new List<ErosionParameters>()
+                {
+                    new ErosionParameters()
+                    {
+                        Latitude = 47.053055,
+                        Longitude = -117.24074,
+                        RotationName = "Transition_NoTill",
+                        WaterErosion = 0.998857975006103,
+                        WaterFO = 0.840264022350311,
+                        WaterOM = 1.05330002307891,
+                        WindErosion = 1,
+                        WindFO = 0.840264022350311,
+                        WindOM = 1.05330002307891
+                    }
+                };
+
+            return parameters;
+        }
+    }
+}
--- src/dotnet/Csip.Scenario/SciBuilder.cs
+++ src/dotnet/Csip.Scenario/SciBuilder.cs
@@ -1,9 +1,83 @@
-﻿namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Scenario
+﻿using Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Common.Models.Files;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Caf.Projects.CafModelingRegionalSoilConditioningIndex.Csip.Scenario
 {
     /// <summary>
     /// Builds scenarios for CSIP SCI service
     /// </summary>
     public class SciBuilder
     {
+        public string GetTemplate()
+        {
+            string template =
+                File.ReadAllText(@"Assets\templates\templateSci.json");
+
+            return template;
+        }
+
+        public List<string> BuildScenarios(
+            List<ErosionParameters> erosionParametersList,
+            string templateJson)
+        {
+            List<string> scenarios = new List<string>();
+
+            JObject jsonObj = JObject.Parse(templateJson);
+
+            foreach(ErosionParameters erosionParameters in erosionParametersList)
+            {
+                string scenario =
+                    AddParameters(jsonObj, erosionParameters)
+                    .ToString();
+
+                scenarios.Add(scenario);
+            }
+            
+            return scenarios;
+        }
+        
+        private JObject AddParameters(
+            JObject scenario,
+            ErosionParameters erosionParameters)
+        {
+            JObject withParameters = scenario;
+
+            // Note: The components of the name are split using double underscore (__), not single, due to an underscore being used in RotationName
+            string scenarioName =
+                $"{erosionParameters.Latitude.ToString("#.####")}__{erosionParameters.Longitude.ToString("#.####")}__{erosionParameters.RotationName}";
+
+            // Set name
+            withParameters["metainfo"]["name"] = scenarioName;
+
+            foreach(var p in withParameters["parameter"])
+            {
+                string paramName = p["name"].ToString();
+                switch(paramName)
+                {
+                    case "erosion_water":
+                        p["value"] = erosionParameters.WaterErosion;
+                        break;
+                    case "water_om":
+                        p["value"] = erosionParameters.WaterOM;
+                        break;
+                    case "water_fo":
+                        p["value"] = erosionParameters.WaterFO;
+                        break;
+                    case "wind_om":
+                        p["value"] = erosionParameters.WindOM;
+                        break;
+                    case "wind_fo":
+                        p["value"] = erosionParameters.WindFO;
+                        break;
+                    case "erosion_wind":
+                        p["value"] = erosionParameters.WindErosion;
+                        break;
+                }
+            }
+
+            return withParameters;
+        }
     }
 }
